017d5904186c309c00e0f98be2b17ffe9d0ba87f
zongqiooi <huro710@hotmail.com>
2023-07-21 17:39:59 +0800

    Added 5 rounds per game and final winner of game

--- odin-rock-paper-scissors/js/rock-paper-scissor.js
+++ odin-rock-paper-scissors/js/rock-paper-scissor.js
@@ -14,19 +14,44 @@ function playRound(playerSelection, computerSelection) {
     let computerSelectionModified = computerSelection.slice(0, 1).toUpperCase() + computerSelection.slice(1); 
     
     if (playerSelection === computerSelection) {
-        return "It is a Draw!"
+        console.log("It is a Draw!"); 
+        return 0 
     } else if ((playerSelection === "rock" && computerSelection === "scissor") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissor" && computerSelection === "paper")) {
-        return `You Win! ${playerSelectionModified} beats ${computerSelectionModified}!`; 
+        console.log(`You Win! ${playerSelectionModified} beats ${computerSelectionModified}!`); 
+        return 1
     } else {
-        return `You Lose! ${computerSelectionModified} beats ${playerSelectionModified}!`; 
+        console.log(`You Lose! ${computerSelectionModified} beats ${playerSelectionModified}!`); 
+        return -1 
     }
 }
 
-let playerSelection = getPlayerChoice(); 
-let computerSelection = getComputerChoice(); 
-let result = playRound(playerSelection, computerSelection); 
+function game() {
+    let playerScore = 0; 
+    let computerScore = 0; 
 
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = getPlayerChoice(); 
+        let computerSelection = getComputerChoice(); 
+        let result = playRound(playerSelection, computerSelection); 
 
-console.log(playerSelection); 
-console.log(computerSelection); 
-console.log(result); 
+        if (result === 0) {
+            console.log(`Round ${i + 1} result: It is a Draw!`); 
+        } else if (result === 1) {
+            console.log(`Round ${i + 1} result: Player Wins!`); 
+            playerScore += 1;
+        } else {
+            console.log(`Round ${i + 1} result: Computer Wins!`); 
+            computerScore += 1; 
+        }
+    }
+
+    if (playerScore > computerScore) {
+        console.log("Final Winner: Player!");
+    } else if (computerScore > playerScore) {
+        console.log("Final Winner: Computer!");
+    } else {
+        console.log("Final Winner: It is a Draw!"); 
+    }
+}
+
+game(); 
