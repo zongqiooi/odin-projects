65b8ea92f919ca55aadaf51744b2f92078635a32
zongqiooi <huro710@hotmail.com>
2023-07-22 18:48:06 +0800

    Added UI for instruction, rounds, and result

--- odin-rock-paper-scissors/rock-paper-scissor.js
+++ odin-rock-paper-scissors/rock-paper-scissor.js
@@ -13,15 +13,19 @@ function playRound(playerSelection, computerSelection) {
     let playerSelectionModified = playerSelection.slice(0, 1).toUpperCase() + playerSelection.slice(1); 
     let computerSelectionModified = computerSelection.slice(0, 1).toUpperCase() + computerSelection.slice(1); 
     let roundResult = document.querySelector(".round-result"); 
+    let instruction = document.querySelector(".instruction"); 
 
     if (playerSelection === computerSelection) {
         roundResult.textContent = "It is a Draw!"; 
+        instruction.textContent = `${playerSelectionModified} ties with ${computerSelectionModified}!`
         return 0;
     } else if ((playerSelection === "rock" && computerSelection === "scissor") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissor" && computerSelection === "paper")) {
-        roundResult.textContent = `You Win! ${playerSelectionModified} beats ${computerSelectionModified}!`; 
+        roundResult.textContent = `You Win!`; 
+        instruction.textContent = `${playerSelectionModified} beats ${computerSelectionModified}!`; 
         return 1;
     } else {
-        roundResult.textContent = `You Lose! ${computerSelectionModified} beats ${playerSelectionModified}!`; 
+        roundResult.textContent = `You Lose!`; 
+        instruction.textContent = `${computerSelectionModified} beats ${playerSelectionModified}!`; 
         return -1; 
     }
 }
@@ -35,7 +39,7 @@ function game() {
     buttons.forEach((button) => {
         button.addEventListener("click", () => {
             let finalResult = document.querySelector(".final-result");
-            
+
             // Game reset when player or computer wins 
             if (playerScore === 5 || computerScore === 5) {
                 playerScore = 0; 
@@ -47,7 +51,8 @@ function game() {
             let playerSelection = button.className; 
             let computerSelection = getComputerChoice(); 
             let result = playRound(playerSelection, computerSelection);
-            let runningScore = document.querySelector(".running-score"); 
+            let playerScoreDiv = document.querySelector(".player-score"); 
+            let computerScoreDiv = document.querySelector(".computer-score"); 
 
             if (result === 1) {
                 playerScore += 1;
@@ -55,15 +60,15 @@ function game() {
                 computerScore += 1; 
             }
             
-            console.log("player score " + playerScore); 
-            console.log("computer score " + computerScore); 
-
-            runningScore.textContent = `Player Score: ${playerScore}   Computer Score: ${computerScore}`; 
+            playerScoreDiv.textContent = `Player Score: ${playerScore}`; 
+            computerScoreDiv.textContent = `Computer Score: ${computerScore}`; 
 
             if (playerScore === 5) {
                 finalResult.textContent = "Final Winner: Player!"; 
+                finalResult.style.color = "green"; 
             } else if (computerScore === 5) {
                 finalResult.textContent = "Final Winner: Computer!";
+                finalResult.style.color = "red"; 
             }
 
             round += 1; 
