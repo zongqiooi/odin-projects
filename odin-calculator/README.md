07a1a87b0b2e947495f89463d49deeb6d696f068
zongqiooi <huro710@hotmail.com>
2023-07-23 18:59:25 +0800

    Added operations and keyboard support

---
+++ odin-calculator/README.md
@@ -0,0 +1,2 @@
+# Project 5 (HTML, CSS, JavaScript): Calculator
+This is the submission files for the project built for JavaScript lesson in Foundations course. 
---
+++ odin-calculator/calculator.js
@@ -0,0 +1,146 @@
+let numStack = [];
+let currOperation; 
+let currNum = ""; 
+let numbers = document.querySelectorAll(".number"); 
+let display = document.querySelector(".calculator-display"); 
+let operators = document.querySelectorAll(".operator"); 
+let clearBtn = document.querySelector(".clear"); 
+
+function add(a, b) {
+    let total = 0; 
+    total = a + b; 
+    return total; 
+}
+
+function subtract(a, b) {
+    total = a - b; 
+    return total; 
+}
+
+function multiply(a, b) {
+    total = a * b; 
+    return total; 
+}
+
+function divide(a, b) {
+    total = a / b; 
+    return total; 
+}
+
+function operate(operation, a, b) {
+    if (operation === "+") {
+        return add(a, b); 
+    } else if (operation === "-") {
+        return subtract(a, b); 
+    } else if (operation === "*") {
+        return multiply(a, b); 
+    } else if (operation === "/") {
+        return divide(a, b); 
+    }
+}
+
+function clear() {
+    currNum = ""; 
+    numStack = [];
+    currOperation = ""; 
+    display.textContent = ""; 
+}
+
+numbers.forEach((number) => {
+    number.addEventListener("click", () => {
+        if ((number.textContent !== ".") || (number.textContent === "." && currNum.includes(".") === false)) {
+            currNum += number.textContent; 
+        }
+        display.textContent = currNum; 
+    })
+})
+
+operators.forEach((operator) => {
+    operator.addEventListener("click", () => {
+        if ((operator.textContent !== "=" && currNum !== "") && numStack.length < 1) {
+            numStack.push(parseFloat(currNum)); 
+            currNum = ""; 
+            currOperation = operator.textContent; 
+        } else if ((operator.textContent === "=" && numStack.length > 0) || (operator.textContent !== "=" && currNum !== "")){
+            numStack.push(parseFloat(currNum)); 
+            currNum = ""; 
+            
+            let result;
+            let b = numStack.pop(); 
+            let a = numStack.pop(); 
+            
+            if (currOperation === "/" && b === 0) {
+                alert("You can't divide by 0!");
+                currNum = ""; 
+                numStack = [];
+                currOperation = "";  
+            } else {
+                result = operate(currOperation, a, b);
+                numStack.push(parseFloat(result)); 
+
+                if (result.toString().includes(".") === true) {
+                    display.textContent = result.toFixed(3); 
+                } else {
+                    display.textContent = result; 
+                }
+                
+                if (operator.textContent !== "=") {
+                    currOperation = operator.textContent; 
+                }    
+            }
+        } else {
+            currOperation = operator.textContent; 
+        } 
+    })
+})
+
+clearBtn.addEventListener("click", () => {
+    clear(); 
+})
+
+document.addEventListener("keydown", (event) => {
+    var name = event.key;
+
+    if (name >= "0" && name <= "9" || name === ".") {
+        if ((name !== ".") || (name === "." && currNum.includes(".") === false)) {
+            currNum += name; 
+        }
+        display.textContent = currNum; 
+    } else if (name === "+" || name === "-" || name === "*" || name === "/" || name === "=" || name === "Enter") {
+        if ((name !== "=" && currNum !== "") && numStack.length < 1) {
+            numStack.push(parseFloat(currNum)); 
+            currNum = ""; 
+            currOperation = name; 
+        } else if ((name === "=" && numStack.length > 0) || (name !== "=" && currNum !== "")){
+            numStack.push(parseFloat(currNum)); 
+            currNum = ""; 
+            
+            let result;
+            let b = numStack.pop(); 
+            let a = numStack.pop(); 
+            
+            if (currOperation === "/" && b === 0) {
+                alert("You can't divide by 0!");
+                currNum = ""; 
+                numStack = [];
+                currOperation = "";  
+            } else {
+                result = operate(currOperation, a, b);
+                numStack.push(parseFloat(result)); 
+
+                if (result.toString().includes(".") === true) {
+                    display.textContent = result.toFixed(3); 
+                } else {
+                    display.textContent = result; 
+                }
+                
+                if (name !== "=") {
+                    currOperation = name; 
+                }    
+            }
+        } else {
+            currOperation = name; 
+        } 
+    }
+}, false);
+
