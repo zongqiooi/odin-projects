c92036a95f0c043fe99943a221df5aea71173f78
zongqiooi <huro710@hotmail.com>
2023-07-28 01:10:22 +0800

    Fixed forms margin for UI

--- odin-sign-up-form/form.js
+++ odin-sign-up-form/form.js
@@ -3,9 +3,6 @@ let confirmPassword = document.querySelector("#confirm_password");
 let errorMessage = document.querySelector(".error-message"); 
 
 confirmPassword.addEventListener("keyup", () => {
-    console.log(password.value); 
-    console.log(confirmPassword.value); 
-
     if (password.value !== confirmPassword.value) {
         errorMessage.style.display = "block"; 
     } else {
