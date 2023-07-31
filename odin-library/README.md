0d06d132ff8147c28aaf55f6c8206e8627f77898
zongqiooi <huro710@hotmail.com>
2023-07-31 15:44:30 +0800

    Added UI for book card

---
+++ odin-library/README.md
@@ -0,0 +1,2 @@
+# Project 8 (JavaScript): Library
+This is the submission files for the project built for JavaScript lesson in JavaScript course. 
--- odin-library/library.js
+++ odin-library/library.js
@@ -27,17 +27,25 @@ function loadBooksFromLibrary() {
         let bookTitle = document.createElement("div"); 
         let bookAuthor = document.createElement("div"); 
         let bookPages = document.createElement("div"); 
-        let bookRead = document.createElement("div"); 
+        let buttonDiv = document.createElement("div"); 
+        let bookRead = document.createElement("button"); 
+        let bookRemove = document.createElement("button"); 
         
-        bookTitle.textContent = book.title;
+        buttonDiv.className = "card-buttons-group";
+        bookTitle.textContent = '"' + book.title + '"';
         bookAuthor.textContent = book.author;
-        bookPages.textContent = book.pages;
-        bookRead.textContent = book.read;
+        bookPages.textContent = book.pages + " pages";
+        bookRead.textContent = book.read ? "Read":"Not Read";
+        bookRead.style.backgroundColor = book.read ? "#9fff9c":"#ff9c9c";
+        bookRead.className = "read-button"; 
+        bookRemove.textContent = "Remove";
         
         bookTag.appendChild(bookTitle);
         bookTag.appendChild(bookAuthor);
         bookTag.appendChild(bookPages);
-        bookTag.appendChild(bookRead);
+        buttonDiv.appendChild(bookRead);
+        buttonDiv.appendChild(bookRemove);
+        bookTag.appendChild(buttonDiv); 
 
         booksContainer.appendChild(bookTag); 
     }); 
@@ -45,3 +53,16 @@ function loadBooksFromLibrary() {
 
 addBookToLibrary(); 
 loadBooksFromLibrary(); 
+
+let buttonGroups = document.querySelectorAll(".card-buttons-group");
+
+buttonGroups.forEach((buttonGroup) => {
+    let readButton = buttonGroup.firstChild; 
+    readButton.addEventListener("click", () => {
+        let buttonColor = readButton.style.backgroundColor; 
+        let buttonText = readButton.textContent; 
+        readButton.style.backgroundColor = (buttonColor === "rgb(159, 255, 156)") ? "#ff9c9c":"rgb(159, 255, 156)"; 
+        readButton.textContent = (buttonText === "Read") ? "Not Read":"Read"; 
+        
+    });
+}); 
