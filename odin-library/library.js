71dbcc4c7f0fab106584098b5365997d5c498ba6
zongqiooi <huro710@hotmail.com>
2023-08-02 16:07:03 +0800

    Added prototype function to toggle read status

--- odin-library/library.js
+++ odin-library/library.js
@@ -8,22 +8,95 @@ function Book(title, author, pages, read, id) {
     this.id = id
 }
 
-function addBookToLibrary() {
-    let book = new Book("Harry Potter", "zongqiooi", "1000", true, 1);
-    let bookOne = new Book("Wassup", "zongqiooi", "1000", true, 2);
-    let bookTwo = new Book("Yoyoyo", "zongqiooi", "1000", true, 3);
-    let bookThr = new Book("hehehe", "zongqiooi", "1000", true, 4);
-    let bookFour = new Book("hahaha", "zongqiooi", "1000", true, 5);
-    let bookFive = new Book("hohoho", "zongqiooi", "1000", true, 6);
-    let bookSix = new Book("huhuhu", "zongqiooi", "1000", true, 7);
+Book.prototype.toggleRead = function() {
+    console.log("wtfff"); 
+    this.read = !this.read; 
+}
 
-    myLibrary.push(book);
+function updateBooks() {
+    let booksCards = document.querySelectorAll(".book-card"); 
+    let booksContainer = document.querySelector(".books-grid"); 
+    
+    booksCards.forEach((bookCard) => {
+        bookCard.remove(); 
+    }); 
+
+    myLibrary.forEach((book) => {
+        let bookTag = document.createElement("div"); 
+        bookTag.classList.add("book-card"); 
+        bookTag.setAttribute("id", `${book.id}`); 
+
+        let bookTitle = document.createElement("div"); 
+        let bookAuthor = document.createElement("div"); 
+        let bookPages = document.createElement("div"); 
+        let buttonDiv = document.createElement("div"); 
+        let bookRead = document.createElement("button"); 
+        let bookRemove = document.createElement("button"); 
+        
+        buttonDiv.className = "card-buttons-group";
+        bookTitle.textContent = '"' + book.title + '"';
+        bookAuthor.textContent = book.author;
+        bookPages.textContent = book.pages + " pages";
+        bookRead.textContent = book.read ? "Read":"Not Read";
+        bookRead.style.backgroundColor = book.read ? "#9fff9c":"#ff9c9c";
+        bookRead.className = "read-button"; 
+        bookRemove.className = "remove-button"
+        bookRemove.textContent = "Remove";
+        
+        bookTag.appendChild(bookTitle);
+        bookTag.appendChild(bookAuthor);
+        bookTag.appendChild(bookPages);
+        buttonDiv.appendChild(bookRead);
+        buttonDiv.appendChild(bookRemove);
+        bookTag.appendChild(buttonDiv); 
+
+        booksContainer.appendChild(bookTag); 
+    });
+
+    let bookCards = document.querySelectorAll(".book-card"); 
+    
+    bookCards.forEach((bookCard) => {
+        let removeButton = bookCard.querySelector(".remove-button"); 
+
+        removeButton.addEventListener("click", () => {
+            myLibrary = myLibrary.filter((book) => {
+                return bookCard.id != book.id; 
+            });
+
+            // Re-assigning the books index
+            myLibrary.forEach((book, index) => {
+                book.id = index; 
+            }); 
+
+            updateBooks();
+        }); 
+
+        let readButton = bookCard.querySelector(".read-button");
+        
+        readButton.addEventListener("click", () => {
+            myLibrary.forEach((book) => {
+                if (bookCard.id == book.id) {
+                    book.toggleRead();
+                    let buttonColor = readButton.style.backgroundColor; 
+                    let buttonText = readButton.textContent; 
+                    readButton.style.backgroundColor = (buttonColor === "rgb(159, 255, 156)") ? "#ff9c9c":"rgb(159, 255, 156)"; 
+                    readButton.textContent = (buttonText === "Read") ? "Not Read":"Read";  
+                    updateBooks();
+                }
+            }); 
+            
+        });
+    });
+}
+
+function addBookToLibrary() {
+    let bookOne = new Book("Harry Potter", "zongqiooi", "900", true, 1);
+    let bookTwo = new Book("Atomic Habits", "zongqiooi", "500", false, 2);
+    let bookThree = new Book("Legends of Goblins", "zongqiooi", "600", true, 3);
+
     myLibrary.push(bookOne);
     myLibrary.push(bookTwo);
-    myLibrary.push(bookThr);
-    myLibrary.push(bookFour);
-    myLibrary.push(bookFive);
-    myLibrary.push(bookSix);
+    myLibrary.push(bookThree);
 }
 
 function loadBooksFromLibrary() {
@@ -66,19 +139,6 @@ function loadBooksFromLibrary() {
         booksContainer.appendChild(bookTag); 
     }); 
 
-    let buttonGroups = document.querySelectorAll(".card-buttons-group");
-
-    buttonGroups.forEach((buttonGroup) => {
-        let readButton = buttonGroup.firstChild; 
-        readButton.addEventListener("click", () => {
-            let buttonColor = readButton.style.backgroundColor; 
-            let buttonText = readButton.textContent; 
-            readButton.style.backgroundColor = (buttonColor === "rgb(159, 255, 156)") ? "#ff9c9c":"rgb(159, 255, 156)"; 
-            readButton.textContent = (buttonText === "Read") ? "Not Read":"Read"; 
-            
-        });
-    }); 
-
     let cross = document.querySelector(".cross-button"); 
     let modal = document.querySelector(".modal"); 
     let overlay = document.querySelector(".overlay"); 
@@ -99,19 +159,6 @@ function loadBooksFromLibrary() {
         cross.classList.add("hide"); 
     });
 
-    let booksCardss = document.querySelectorAll(".book-card"); 
-
-    booksCardss.forEach((booksCard) => {
-        let removeButton = booksCard.querySelector(".remove-button"); 
-
-        removeButton.addEventListener("click", () => {
-            myLibrary = myLibrary.filter((book) => {
-                return booksCard.id != book.id; 
-            })
-            loadBooksFromLibrary();
-        }); 
-    });
-
     let addBookForm = document.querySelector("#add-book-form"); 
 
     addBookForm.addEventListener("submit", (event) => {
@@ -121,19 +168,57 @@ function loadBooksFromLibrary() {
         let author = document.querySelector("#author").value; 
         let pages = document.querySelector("#pages").value; 
         let checkbox = document.querySelector("#checkbox").checked; 
-        let id = myLibrary.length() + 1; 
+        let id = myLibrary.length + 1; 
         let book = new Book(title, author, pages, checkbox, id);
-        
-        console.log(title);
-        console.log(author);
-        console.log(pages);
-        console.log(checkbox);
-        console.log(id);
-        myLibrary.push(book);
-        loadBooksFromLibrary(); 
+
+        myLibrary = [...myLibrary, book]; 
+
+        // Re-assigning the books index
+        myLibrary.forEach((book, index) => {
+            book.id = index; 
+        }); 
+
+        modal.classList.add("hide"); 
+        overlay.classList.add("hide"); 
+        cross.classList.add("hide"); 
+
+        updateBooks(); 
     }); 
+
+    let bookCards = document.querySelectorAll(".book-card"); 
+    
+    bookCards.forEach((bookCard) => {
+        let removeButton = bookCard.querySelector(".remove-button"); 
+
+        removeButton.addEventListener("click", () => {
+            myLibrary = myLibrary.filter((book) => {
+                return bookCard.id != book.id; 
+            }); 
+
+            // Re-assigning the books index
+            myLibrary.forEach((book, index) => {
+                book.id = index; 
+            }); 
+
+            updateBooks();
+        }); 
+
+        let readButton = bookCard.querySelector(".read-button");
+        
+        readButton.addEventListener("click", () => {
+            myLibrary.forEach((book) => {
+                if (bookCard.id == book.id) { 
+                    book.toggleRead();
+                    let buttonColor = readButton.style.backgroundColor; 
+                    let buttonText = readButton.textContent; 
+                    readButton.style.backgroundColor = (buttonColor === "rgb(159, 255, 156)") ? "#ff9c9c":"rgb(159, 255, 156)"; 
+                    readButton.textContent = (buttonText === "Read") ? "Not Read":"Read";  
+                    updateBooks();
+                }
+            }); 
+        });
+    });
 }
 
 addBookToLibrary(); 
 loadBooksFromLibrary(); 
-
