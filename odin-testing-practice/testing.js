ae09f1468d2c5465d9a3919c04ab158a4929a2ed
zongqiooi <huro710@hotmail.com>
2023-08-17 16:01:43 +0800

    Implemented test cases for Caesar Cipher and analyzeArr

--- odin-testing-practice/testing.js
+++ odin-testing-practice/testing.js
@@ -1,8 +1,8 @@
-function capitalize(s) {
+const capitalize = (s) => {
   return s.slice(0, 1).toUpperCase() + s.slice(1, s.length);
-}
+};
 
-function reverseString(s) {
+const reverseString = (s) => {
   let reversedString = "";
 
   for (let i = s.length - 1; i >= 0; i--) {
@@ -11,7 +11,7 @@ function reverseString(s) {
 
   console.log(reversedString);
   return reversedString;
-}
+};
 
 const calculator = {
   add: (num1, num2) => num1 + num2,
@@ -20,4 +20,109 @@ const calculator = {
   multiply: (num1, num2) => num1 * num2,
 };
 
-module.exports = { capitalize, reverseString, calculator };
+const caesarCipher = (s, shiftFactor) => {
+  const lowerCase = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  const upperCase = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+
+  let convertedString = "";
+  let strIdx = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    // Lowercase; Uppercase; Symbols and spaces
+    if (/[a-zA-Z]/.test(s[i]) && s[i].toLowerCase() === s[i]) {
+      strIdx = lowerCase.indexOf(s[i]);
+      convertedString += lowerCase[(strIdx + shiftFactor) % 26];
+    } else if (/[a-zA-Z]/.test(s[i]) && s[i].toUpperCase() === s[i]) {
+      strIdx = upperCase.indexOf(s[i]);
+      convertedString += upperCase[(strIdx + shiftFactor) % 26];
+    } else {
+      convertedString += s[i];
+    }
+  }
+
+  return convertedString;
+};
+
+const analyzeArray = (nums) => {
+  let total = 0;
+  let average = 0;
+  let min = Infinity;
+  let max = -Infinity;
+  let length = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    total += nums[i];
+    if (nums[i] < min) {
+      min = nums[i];
+    }
+    if (nums[i] > max) {
+      max = nums[i];
+    }
+  }
+
+  length = nums.length;
+  average = total / length;
+  return { average, min, max, length };
+};
+
+module.exports = {
+  capitalize,
+  reverseString,
+  calculator,
+  caesarCipher,
+  analyzeArray,
+};
--- odin-testing-practice/testing.test.js
+++ odin-testing-practice/testing.test.js
@@ -1,4 +1,10 @@
-const { capitalize, reverseString, calculator } = require("./testing");
+const {
+  capitalize,
+  reverseString,
+  calculator,
+  caesarCipher,
+  analyzeArray,
+} = require("./testing");
 
 it("Capitalizing 'ashleynguci' to 'Ashleynguci'", () => {
   expect(capitalize("ashleynguci")).toBe("Ashleynguci");
@@ -22,4 +28,37 @@ it("Divide 6 / 4 to 1.5 ", () => {
 
 it("Multiply 6 * 4 to 24 ", () => {
   expect(calculator.multiply(6, 4)).toEqual(24);
+});
+
+it("Caesar cipher of 'abc' shifted by 0 to be 'abc'", () => {
+  expect(caesarCipher("abc", 0)).toEqual("abc");
+});
+
+it("Caesar cipher of 'abc' shifted by 1 to be 'bcd'", () => {
+  expect(caesarCipher("abc", 1)).toEqual("bcd");
+});
+
+it("Caesar cipher of 'az' shifted by 5 to be 'fe'", () => {
+  expect(caesarCipher("az", 5)).toEqual("fe");
+});
+
+it("Caesar cipher of 'a, b, c' shifted by 1 to be 'b, c, d'", () => {
+  expect(caesarCipher("a, b, c", 1)).toEqual("b, c, d");
+});
+
+it("Caesar cipher of 'ABc' shifted by 3 to be 'DEf'", () => {
+  expect(caesarCipher("ABc", 3)).toEqual("DEf");
+});
+
+it("Caesar cipher of 'aB, c' shifted by 3 to be 'dE, f'", () => {
+  expect(caesarCipher("aB, c", 3)).toEqual("dE, f");
+});
+
+it("Analyze the array of [1,8,3,4,2,6] to be {average: 4, min: 1, max: 8, length: 6}", () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
 });
